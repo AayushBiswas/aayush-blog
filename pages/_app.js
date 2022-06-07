f40e3a0a48086ecadda4cb6aaa5941a1a6d32fef
Aayush Biswas <70165265+AayushBiswas@users.noreply.github.com>
2022-06-07 22:29:28 +0530

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,28 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import React, { useState, useEffect } from "react";
 import { Layout } from "../components";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script 
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-Z9SZ2SQ11X`}
+      />
+      <Script 
+        strategy="lazyOnLoad"
+      >
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z9SZ2SQ11X');
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
